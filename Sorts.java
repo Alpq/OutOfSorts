3d028f35a4442f1e81e99c8ffe32765ab0950a48
Alpq <doymazalp@gmail.com>
2020-12-18 09:20:46 -0500

    Implemented bubble sort method in the Sort file. File compiles. Moving onto testing.

---
+++ Sorts.java
@@ -0,0 +1,22 @@
+public class Sorts{
+  /**Bubble sort of an int array.
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void bubbleSort(int[] data)
+  {
+    for (int j = 0; j < data.length - 1; j++)
+    {
+      for (int i = 0 ;i < data.length - i ; i ++)
+      {
+          int temp;
+          if (data[i] > data[i + 1])
+          {
+            temp = data[i + 1];
+            data[i + 1] = data[i];
+            data[i] = temp;
+          }
+      }
+    }
+  }
+}
