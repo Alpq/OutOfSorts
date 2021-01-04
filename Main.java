f197c0486a851ab4d20fe6089c117a94d2c47a17
Alpq <doymazalp@gmail.com>
2021-01-04 09:06:50 -0500

    Added insertionsort(). The function compiles and tests properly.

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
 import java.util.Random;
 public class Main{
   public static void main(String[] args) {
-    int[] wow = new int[100];
+    int[] wow = new int[101];
     for (int i = 0 ;i < wow.length ; i ++ ) {
       {
-        wow[i] = (new Random(1000)).nextInt();
+        //wow[i] = (new Random()).nextInt();
+        wow[i] = (new Random()).nextInt() ;
       }
     }
     String s = "{ ";
-    Sorts.bubbleSort(wow);
+    for (int i = 0; i < wow.length ;i ++ ) {
+      s = s + Integer.toString(wow[i]) + ", ";
+    }
+    s = s + " }";
+    System.out.println(s);
+    System.out.println();
+    System.out.println();
+     s = "{ ";
+    Sorts.insertionSort(wow);
     for (int i = 0; i < wow.length ;i ++ ) {
       s = s + Integer.toString(wow[i]) + ", ";
     }
--- Sorts.java
+++ Sorts.java
@@ -20,4 +20,38 @@ public class Sorts{
       }
     }
   }
+  public static void selectionSort(int[] data){
+    int temp;
+    for (int i = 0; i < data.length; i ++ ) {
+      int form = i;
+      int min = data[i];
+      for (int j = i; j < data.length; j++ ) {
+        if (data[j] < min) {form = j;}
+        min = Math.min(data[j], min);
+      }
+      temp = data[i];
+      data[i] = min;
+      data[form] = temp;
+    }
+  }
+  public static void insertionSort(int[] data){
+    int temp;
+    for (int i = 1; i < data.length; i ++ ) {
+      if (data[i-1] > data[i])
+      {
+        temp = data[i];
+        boolean notfound = true;
+        int iterator = i;
+        while (notfound && iterator > 0) {
+          if (data[iterator - 1] < temp)
+          {
+            notfound = false;
+            data[iterator] = temp;
+          }
+          else {data[iterator] = data[iterator - 1];}
+          iterator--;
+        }
+      }
+    }
+  }
 }
